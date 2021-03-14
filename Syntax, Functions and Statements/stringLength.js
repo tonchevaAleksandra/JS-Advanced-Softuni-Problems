0695be3c19eedc82b9908ce063bec64ee4cca17a
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-03-14 19:22:07 +0200

    Add

---
+++ Syntax, Functions and Statements/stringLength.js
@@ -0,0 +1,6 @@
+function stringLength(input1, input2, input3){
+    let sum=input1.length +input2.length+input3.length;
+    console.log(sum);
+    let averageSum = Math.floor(sum/3);
+console.log(averageSum);
+}
