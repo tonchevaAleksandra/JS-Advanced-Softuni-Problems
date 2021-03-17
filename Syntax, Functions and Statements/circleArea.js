41e99e935ac7945d01fdff5e1210ba33233e74ba
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-03-17 19:26:44 +0200

    Add

--- Syntax, Functions and Statements/circleArea.js
+++ Syntax, Functions and Statements/circleArea.js
@@ -1,14 +1,14 @@
-function circleArea(input){
-    let inputType=typeof input;
+function circleArea(input) {
+    let inputType = typeof (input);
 
-    if(inputType==='number')
-    {
-        let result=Math.pow(input);
-    } else{
+    if (inputType === 'number') {
+        let result = Math.pow(input, 2) * Math.PI;
+        console.log(result.toFixed(2));
+    } else {
         console.log(`We can not calculate the circle area, because we receive a ${inputType}.`)
     }
 
 }
 
 circleArea(5.5);
-// circleArea('5');
+circleArea('5');
